7ac37e2c2a6179c5b9079f62c5a9befacb6a2fb1
Chen-Yulin <chenyulin@sjtu.edu.cn>
2021-03-29 23:59:38 +0800

    newly updated

--- servo.ino
+++ servo.ino
@@ -1,13 +1,54 @@
 #include <Servo.h>
-#define PIN_SERVO 10
-Servo myservo;
- 
+float toArmPosition;
+void armreach(float toArmPosition, int speed, int armNum){//armNum:1(left).2:(right); to arm reach:90~30(90:the initial position,30:the furthest postion; speed:255MAX)
+    static float currentArmPositionLeft = 90;
+    static float currentArmPositionRight = 90;
+    if(armNum == 1){
+        if(currentArmPosition > toArmPosition){
+            if(!(millis()%50)){
+                pos2 += 0.004*speed;
+                L5.write((currentArmPositionLeft+pos2)*0.95);
+                L3.write((currentArmPositionLeft+7)-pos2);
+                L4.write((currentArmPositionLeft-2*pos2)*1.1);
+                currentArmPositionLeft -= pos2;
+            }
+        }
+        if(currentArmPosition < toArmPosition){
+            if(!(millis()%50)){
+                pos2 += 0.004*speed;
+                L5.write((currentArmPositionLeft+pos2)*0.95);
+                L3.write((currentArmPositionLeft+7)-pos2);
+                L4.write((currentArmPositionLeft-2*pos2)*1.1);
+                currentArmPositionLeft += pos2;
+            }
+        }
+    }
+    if(armNum == 2){
+        if(currentArmPosition > toArmPosition){
+            if(!(millis()%50)){
+                pos2 += 0.004*speed;
+                L().write((currentArmPositionRight()pos2)*0.95);//fill the ()
+                L().write(currentArmPositionRight()pos2);//fill the ()
+                L().write((currentArmPositionRight()2*pos2)*1.1);//fill the ()
+                currentArmPositionRight -= pos2;
+            }
+        }
+        if(currentArmPosition < toArmPosition){
+            if(!(millis()%50)){
+                pos2 += 0.004*speed;
+                L().write((currentArmPositionRight()pos2)*0.95);//fill the ()
+                L().write(currentArmPositionRight()pos2);//fill the ()
+                L().write((currentArmPositionRight()2*pos2)*1.1);//fill the ()
+                currentArmPositionRight += pos2;
+            }
+        }
+    }
+}
 void setup()
 {
-  myservo.attach(3);
 }
  
 void loop()
 {
-myservo.write(90);
+armreach(60,200,1);
 }
