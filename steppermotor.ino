f326a510e0dba4ea67808f7770202f78d9834fb6
Chen-Yulin <chenyulin@sjtu.edu.cn>
2021-03-30 19:11:21 +0800

    newly updated 3/30

--- steppermotor.ino
+++ steppermotor.ino
@@ -1,90 +1,108 @@
-#define dir_r_r 21
-#define pul_r_r 22
+#define pul_l_L 28
+#define dir_l_L 14
+#define pul_l_R 13
+#define dir_l_R 17
+#define pul_r_L 33
+#define dir_r_L 20
+#define pul_r_R 12
+#define dir_r_R 23
+#define pul_h_1 11
+#define dir_h_1 41
+#define pul_h_2 10
+#define dir_h_2 45
 
-int i = 1;
-int current_right_ange;
 
-struct 
-{
-    int part;
-    int movement;
-    int position;
-} piinf;
 
-void setup()
+void steppermotor(int to_angle,int motorNum)
 {
-    Serial.begin(9600);
-    pinMode(dir_r_r, OUTPUT);
-    pinMode(pul_r_r, OUTPUT);
-    piinf.part = 0;
-    piinf.movement = 0;
-    piinf.position = 0;
-    
-}
-void steppermotor_right_rotate(int to_angle)
-{
+    static int pul;
+    static int dir;
+    static float current_angle;
+    static int current_dir;
+    static float current_angle_l_L = 0;
+    static float current_angle_l_R = 0;
+    static float current_angle_r_L = 0;
+    static float current_angle_r_R = 0;
+    static float current_position_h = 0;
+
+    static int current_dir_l_L = -1;
+    static int current_dir_l_R = -1;
+    static int current_dir_r_L = -1;
+    static int current_dir_r_R = -1;
+    static int current_dir_h = -1;
+
+    if(motorNum == 1) {pul = pul_l_L;dir = dir_l_L;current_dir = current_dir_l_L;current_angle = current_angle_l_L;}
+    if(motorNum == 2) {pul = pul_l_R;dir = dir_l_R;current_dir = current_dir_l_R;current_angle = current_angle_l_R;}
+    if(motorNum == 3) {pul = pul_r_L;dir = dir_r_L;current_dir = current_dir_r_L;current_angle = current_angle_r_L;}
+    if(motorNum == 4) {pul = pul_r_R;dir = dir_r_R;current_dir = current_dir_r_R;current_angle = current_angle_r_R;}
+    if(motorNum == 5) {pul = pul_h_1;dir = dir_h_1;current_dir = current_dir_h;current_angle = current_position_h;}
+
+    static int i[5] = {1,1,1,1,1};
     static int to_dir;
-    if(to_angle - current_right_ange > 0) to_dir = 1;
-    if(to_angle - current_right_ange < 0) to_dir = 0;
-    static int current_dir = to_dir;
+    if(to_angle - current_angle > 0) to_dir = 1;
+    if(to_angle - current_angle < 0) to_dir = 0;
+
     static unsigned long time = 0;
-    if(current_dir != to_dir && current_dir != -1)
-    {
-        digitalWrite(pul_r_r, 0);
-        if((micros()-time) > 1000000)
-        {
+
+    if(current_dir != to_dir && current_dir != -1){
+        digitalWrite(pul, 0);
+        if((micros()-time) > 1000000){
             current_dir = to_dir;
             time = micros();
         }
     }
     else
     {
-        if(to_dir == 1)
-        {
-        
-            digitalWrite(dir_r_r, 1);
+        if(to_dir == 1){
+            digitalWrite(dir, 1);
             current_dir = 1;
-            if((micros() - time) >= 150)
-            {
-                digitalWrite(pul_r_r, i%2);
-                current_right_ange = current_right_ange + (i%2 == 1)*0.225;
-                i += 1;
+            if((micros() - time) >= 250){
+                digitalWrite(pul, i[motorNum]%2);
+                current_angle = current_angle + (i[motorNum]%2 == 1)*0.225;
+                i[motorNum] += 1;
                 time = micros();
             } 
         }
-        if(to_dir == 0)
-        {
-            digitalWrite(dir_r_r, 0);
+        if(to_dir == 0){
+            digitalWrite(dir, 0);
             current_dir = 0;
-            if((micros() - time) >= 150)
-            {
-                digitalWrite(pul_r_r, i%2);
-                current_right_ange = current_right_ange - (i%2 == 1)*0.225;
-                i += 1;
+            if((micros() - time) >= 250){
+                digitalWrite(pul, i[motorNum]%2);
+                current_angle = current_angle - (i[motorNum]%2 == 1)*0.225;
+                i[motorNum] += 1;
                 time = micros();
             } 
         }
-        if(to_angle-current_right_ange == 0)
-        {
-            i = 1;
+        if(to_angle-current_angle == 0){
+            i[motorNum] = 1;
             current_dir = -1;//此时方向为任意
         }
     }
-
+    if(motorNum == 1) {current_dir_l_L = current_dir;current_angle_l_L = current_angle;}
+    if(motorNum == 2) {current_dir_l_R = current_dir;current_angle_l_R = current_angle;}
+    if(motorNum == 3) {current_dir_r_L = current_dir;current_angle_r_L = current_angle;}
+    if(motorNum == 4) {current_dir_r_R = current_dir;current_angle_r_R = current_angle;}
+    if(motorNum == 5) {current_dir_h = current_dir;current_position_h = current_angle;}
+}
+void setup()
+{
+    pinMode(pul_l_L,OUTPUT);
+    pinMode(dir_l_L,OUTPUT);
+    pinMode(pul_l_R,OUTPUT);
+    pinMode(dir_l_R,OUTPUT);
+    pinMode(pul_r_L,OUTPUT);
+    pinMode(dir_r_L,OUTPUT);
+    pinMode(pul_r_R,OUTPUT);
+    pinMode(dir_r_R,OUTPUT);
+    pinMode(pul_h_1,OUTPUT);
+    pinMode(dir_h_1,OUTPUT);
+    pinMode(pul_h_2,OUTPUT);
+    pinMode(dir_h_2,OUTPUT);
 }
 
-void loop()
-{
-    //收到pi传来的信息（第一位2指右臂，第二位1指旋转，第三位90指旋转移动到的位置)
-    piinf.part = 2;
-    piinf.movement = 1;
-    piinf.position = 5*700;
 
-    if(piinf.part == 2)
-    {
-        if(piinf.movement == 1)
-        {
-            steppermotor_right_rotate(piinf.position);//from 0 to 25*700 (700 steps per cm),一般不输入负数，负数用于测试后的归正
-        }
-    }
+void loop(){
+
+    steppermotor(-1200,1);
+
 }
