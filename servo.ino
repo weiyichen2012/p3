dd2ce9a99ebee404983454c7c055ba2d358cd07f
Chen-Yulin <chenyulin@sjtu.edu.cn>
2021-03-30 19:10:34 +0800

    newly updated 3/30

--- servo.ino
+++ servo.ino
@@ -1,54 +1,125 @@
 #include <Servo.h>
-float toArmPosition;
-void armreach(float toArmPosition, int speed, int armNum){//armNum:1(left).2:(right); to arm reach:90~30(90:the initial position,30:the furthest postion; speed:255MAX)
+
+Servo L1;
+Servo L2;
+Servo L3;
+Servo L4;
+Servo L5;
+Servo R1;
+Servo R2;
+Servo R3;
+Servo R4;
+Servo R5;
+
+void pinout()
+{
+
+L3.attach(7);
+L4.attach(10);
+L5.attach(11);
+R3.attach(4);
+R4.attach(2);
+R5.attach(3);
+L1.attach(9);
+L2.attach(8);
+R1.attach(6);
+R2.attach(5);
+}
+
+void startup()
+{
+L3.write(95);
+L4.write(90);
+L5.write(90);
+R3.write(85);
+R4.write(90);
+R5.write(90);
+L1.write(90);
+L2.write(90);
+R1.write(90);
+R2.write(90);
+
+}
+
+
+void armreach(float toArmPosition, int sped, int armNum){//armNum:1(left).2:(right); to arm reach:90~30(90:the initial position,30:the furthest postion; speed:1/2/4)
     static float currentArmPositionLeft = 90;
     static float currentArmPositionRight = 90;
+    static bool flag = true;
+    float pos2 = 0.0625*sped;
     if(armNum == 1){
-        if(currentArmPosition > toArmPosition){
-            if(!(millis()%50)){
-                pos2 += 0.004*speed;
-                L5.write((currentArmPositionLeft+pos2)*0.95);
-                L3.write((currentArmPositionLeft+7)-pos2);
-                L4.write((currentArmPositionLeft-2*pos2)*1.1);
+        if(currentArmPositionLeft > toArmPosition){
+            if(!(millis()%20) && flag == true){
                 currentArmPositionLeft -= pos2;
+                L5.write(180-currentArmPositionLeft);
+                L3.write(currentArmPositionLeft+5);
+                L4.write(2.1*currentArmPositionLeft-99);
+                flag = false;
+            }
+            if(millis()%50 != 0){
+                flag = true;
             }
         }
-        if(currentArmPosition < toArmPosition){
-            if(!(millis()%50)){
-                pos2 += 0.004*speed;
-                L5.write((currentArmPositionLeft+pos2)*0.95);
-                L3.write((currentArmPositionLeft+7)-pos2);
-                L4.write((currentArmPositionLeft-2*pos2)*1.1);
-                currentArmPositionLeft += pos2;
+        if(currentArmPositionLeft < toArmPosition){
+            if(!(millis()%20) && flag == true){
+              currentArmPositionLeft += pos2;
+                L5.write(180-currentArmPositionLeft);
+                L3.write(currentArmPositionLeft+5);
+                L4.write(2.1*currentArmPositionLeft-99);
+                flag = false;
+            }
+            if(millis()%50 != 0){
+                flag = true;
             }
         }
     }
     if(armNum == 2){
-        if(currentArmPosition > toArmPosition){
-            if(!(millis()%50)){
-                pos2 += 0.004*speed;
-                L().write((currentArmPositionRight()pos2)*0.95);//fill the ()
-                L().write(currentArmPositionRight()pos2);//fill the ()
-                L().write((currentArmPositionRight()2*pos2)*1.1);//fill the ()
-                currentArmPositionRight -= pos2;
+        if(currentArmPositionLeft > toArmPosition){
+            if(!(millis()%20) && flag == true){
+                currentArmPositionLeft -= pos2;
+                R5.write(180-currentArmPositionLeft);
+                R3.write(180-currentArmPositionLeft-5);
+                R4.write(2.1*currentArmPositionLeft-99);
+                flag = false;
+            }
+            if(millis()%50 != 0){
+                flag = true;
             }
         }
-        if(currentArmPosition < toArmPosition){
-            if(!(millis()%50)){
-                pos2 += 0.004*speed;
-                L().write((currentArmPositionRight()pos2)*0.95);//fill the ()
-                L().write(currentArmPositionRight()pos2);//fill the ()
-                L().write((currentArmPositionRight()2*pos2)*1.1);//fill the ()
-                currentArmPositionRight += pos2;
+        if(currentArmPositionLeft < toArmPosition){
+            if(!(millis()%20) && flag == true){
+              currentArmPositionLeft += pos2;
+                R5.write(180-currentArmPositionLeft);
+                R3.write(180-currentArmPositionLeft-5);
+                R4.write(2.1*currentArmPositionLeft-99);
+                flag = false;
+            }
+            if(millis()%50 != 0){
+                flag = true;
             }
         }
     }
+    
 }
 void setup()
 {
+    pinout();
+    startup();
+    delay(2000);
 }
  
 void loop()
-{
-armreach(60,200,1);
+{  
+  //L5.write(100);
+  //delay(1500);
+  //L5.write(90);
+  //delay(1500);
+  if(millis()<6000) armreach(70,2,2);// position,speed(1/2/4),armNum
+  else armreach(90,4,2);
+
+
+
+
+
+
 }
